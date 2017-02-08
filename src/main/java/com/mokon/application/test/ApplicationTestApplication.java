f1ad6f5af1b64dce5bb0945cc7539e964777a9bd
marcinokon <marcin.okonpk@altkom.pl>
2017-02-08 14:28:41 +0100

    Removed unecessary @ComponentScan

--- src/main/java/com/mokon/application/test/ApplicationTestApplication.java
+++ src/main/java/com/mokon/application/test/ApplicationTestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.mokon.*")
 public class ApplicationTestApplication {
 
 	public static void main(String[] args) {
